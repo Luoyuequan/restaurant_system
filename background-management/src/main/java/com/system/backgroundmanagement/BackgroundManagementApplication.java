c77c8f113482caa439eb7c6aed80800dcaf6182f
罗跃权 <1260612638@qq.com>
2020-01-02 21:15:02 +0800

    first build project

---
+++ background-management/src/main/java/com/system/backgroundmanagement/BackgroundManagementApplication.java
@@ -0,0 +1,18 @@
+package com.system.backgroundmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台管理系统启动类
+ *
+ * @author 罗跃权
+ */
+@SpringBootApplication
+public class BackgroundManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundManagementApplication.class, args);
+    }
+
+}
---
+++ front-management/src/main/java/com/system/frontmanagement/FrontManagementApplication.java
@@ -0,0 +1,18 @@
+package com.system.frontmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 前台管理系统启动类
+ *
+ * @author 罗跃权
+ */
+@SpringBootApplication
+public class FrontManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontManagementApplication.class, args);
+    }
+
+}
